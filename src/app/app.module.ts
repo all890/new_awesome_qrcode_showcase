c01292afca8f582dd596ef2b6fb7fe157149b394
all890 <73221724+all890@users.noreply.github.com>
2024-03-22 14:45:17 +0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { QrCodeStyleComponent } from './qr-code-style/qr-code-style.component';
-import { EasyQrJsComponent } from './easy-qr-js/easy-qr-js.component';
-import { ZxingComponent } from './zxing/zxing.component';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @NgModule({
   declarations: [
     AppComponent,
     QrCodeStyleComponent,
-    EasyQrJsComponent,
-    ZxingComponent
   ],
   imports: [
     BrowserModule,
